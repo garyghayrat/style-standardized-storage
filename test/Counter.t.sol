5d025dc7e59ea4f322cd9e9cc8b5ce08de7c8200
Gary Ghayrat <f255eb47-1441-48a5-be8e-0a9e3193245c@anonaddy.me>
2022-09-30 12:20:05 -0400

    Add files following style guide

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -1,8 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import { Test } from "forge-std/Test.sol"; /// Use absolute paths and named imports.
-import { Counter } from "src/Counter.sol";
+import {Test} from "forge-std/Test.sol"; /// Use absolute paths and named imports.
+import {MockERC20} from "solmate/test/utils/mocks/MockERC20.sol";
+import {CounterHarness} from "test/CounterHarness.t.sol";
+import {CounterScript} from "script/CounterScript.s.sol";
+import {Counter} from "src/Counter.sol";
 
 contract CounterTest is Test {
   Counter public counter;
